1addfe2584d0c12d18e4383c06566e4539b2e2d0
xbl <x_bl@sina.com>
2025-08-07 17:34:28 +0800

    feat: 初始化React router 项目，同时增加一个最简单的路由示例

---
+++ react-router-demo/src/About.jsx
@@ -0,0 +1,14 @@
+
+import { Link } from 'react-router'
+
+function About() {
+
+  return (
+    <>
+      <h1>About！！！</h1>
+      <Link to="/">首页</Link>
+    </>
+  )
+}
+
+export default About
---
+++ react-router-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>首页</h1>
+      <Link to="/about">About</Link>|
+    </>
+  )
+}
+
+export default App
