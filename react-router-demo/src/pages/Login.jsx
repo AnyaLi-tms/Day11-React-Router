2a64feef1e1e8ae0c0b36b7d11d7d48f5fd7be24
xbl <x_bl@sina.com>
2025-08-08 18:09:45 +0800

    feat: 添加登录组件

---
+++ react-router-demo/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import useLoginStore from '../stores/appStore.js'
+
+function Login() {
+  const { login, logout } = useLoginStore()
+
+  const handleLogin = () => {
+    login()
+    alert('登录成功')
+  }
+  const handleLogout = () => {
+    logout()
+    alert('退出成功')
+  }
+  return (
+    <div>
+      <h1>这里是登录页面</h1>
+      <p>没有登录的用户会看到这个页面</p>
+      <button onClick={handleLogin}>登录</button>
+      <button onClick={handleLogout}>退出</button>
+    </div>
+  )
+}
+
+export default Login
