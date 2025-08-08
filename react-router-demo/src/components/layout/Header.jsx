b63c366746ec0f08c92fae6e82e7788669b8f407
xbl <x_bl@sina.com>
2025-08-08 16:17:39 +0800

    refactor: Header 样式更新，看起来更像电商网站

--- react-router-demo/src/components/layout/Header.jsx
+++ react-router-demo/src/components/layout/Header.jsx
@@ -1,17 +1,24 @@
-import { Link } from 'react-router'
-import styles from './Header.module.css'
+import { Link } from 'react-router';
+import styles from './Header.module.css';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.inner}>
+      <div className={styles.container}>
+        <Link to='/' className={styles.logo}>
+          不值得买电商
+        </Link>
         <nav className={styles.nav}>
-          <Link to='/' className={styles.link}>首页</Link>
-          <Link to='/about' className={styles.link}>关于</Link>
+          <Link to='/' className={styles.navLink}>
+            首页
+          </Link>
+          <Link to='/about' className={styles.navLink}>
+            关于
+          </Link>
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
