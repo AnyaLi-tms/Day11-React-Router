8a4755538864b7704d93a79240073a6e4643ef74
xbl <x_bl@sina.com>
2025-08-16 21:18:04 +0800

    feat: 增加async 练习

--- promise-demo/01-basic-promise.js
+++ promise-demo/01-basic-promise.js
@@ -6,3 +6,5 @@ function delay(ms) {
 delay(1000)
   .then(message => console.log('基础测试:', message)) // 1秒后输出 "1000毫秒后完成"
   .catch(error => console.error('基础测试错误:', error));
+
+export default delay;
---
+++ promise-demo/03-async-await.js
@@ -0,0 +1,32 @@
+/**
+ * 11-async-await.js
+ * async/await 语法学习
+ * 
+ * 学习目标：
+ * 1. 掌握 async/await 的基本语法
+ * 2. 理解 async/await 与 Promise 的关系
+ * 3. 学会使用 async/await 简化异步代码
+ * 4. 掌握错误处理的最佳实践
+ */
+import delay from './01-basic-promise.js';
+
+console.log('=== async/await 语法学习 ===\n');
+
+// 1. async/await 基本语法
+console.log('1. async/await 基本语法');
+
+// TODO: 将 basicAsync 改为 async 函数，并调用 delay
+function basicAsync() {
+    console.log('  开始执行异步函数');
+    // 等待 Promise 完成
+    delay(1000);
+    // 1秒后输出
+    console.log('  获取到结果:', result);
+    
+    return '函数执行完成';
+}
+
+// 调用异步函数
+basicAsync().then(result => {
+    console.log('  异步函数返回:', result);
+});
