e314959739c1bd63e91c1b55da3b95f570b41eda
xbl <x_bl@sina.com>
2025-08-16 20:43:32 +0800

    feat: 增加 Promise 练习

---
+++ promise-demo/01-basic-promise.js
@@ -0,0 +1,8 @@
+// 练习1：基础封装 - 将setTimeout包装成Promise
+function delay(ms) {
+  // TODO: 补全代码，返回一个Promise
+}
+// 测试基础功能
+delay(1000)
+  .then(message => console.log('基础测试:', message)) // 1秒后输出 "1000毫秒后完成"
+  .catch(error => console.error('基础测试错误:', error));
